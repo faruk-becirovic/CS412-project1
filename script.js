97d840a5aa5d2eb71668b0fb12c9b422f509382d
faruk <faruk.becirovic@zoho.com>
2021-12-09 23:47:57 +0100

    project 3

---
+++ script.js
@@ -0,0 +1,27 @@
+setInterval(showTime, 1000);
+
+function showTime() {
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+
+    let currentTime = hour + ":" + min + ":" + sec;
+
+    document.getElementById("clock").innerHTML = currentTime;
+      }
+
+showTime();
+
+function navigation() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
